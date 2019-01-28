36507e8b0e8b2d4d3a4bebbf4c0bfbc55e20c191
spencercornish <SpencerCornish@users.noreply.github.com>
2019-01-27 17:23:01 -0700

    cleanup

--- export.py
+++ export.py
@@ -5,4 +5,5 @@ today = datetime.today()
 idx = (today.weekday() + 1) % 7
 sun = today - timedelta(7+idx)
 
-system("markdown-pdf {:%m-%d-%Y}.md -o pdfs/{:%m-%d-%Y}.pdf".format(sun, sun))
+system(
+    "markdown-pdf md/{:%m-%d-%Y}.md -o pdfs/{:%m-%d-%Y}.pdf".format(sun, sun))
--- new.py
+++ new.py
@@ -5,13 +5,14 @@ import sys
 today = datetime.today()
 idx = (today.weekday() + 1) % 7
 sun = today - timedelta(7+idx)
+filePath = "md/{:%m-%d-%Y}.md".format(sun)
 
-if os.path.isfile("{:%m-%d-%Y}.md".format(sun)):
+if os.path.isfile(filePath):
     print("ERROR: File already exists! ")
     sys.exit(1)
 
-templateFile = open("TEMPLATE.md", "r")
-newFile = open("{:%m-%d-%Y}.md".format(sun), "w")
+templateFile = open("md/TEMPLATE.md", "r")
+newFile = open(filePath, "w")
 
 for line in templateFile:
     if "%DATE%" in line:
